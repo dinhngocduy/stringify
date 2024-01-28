8e2f2b2fd2cebed50ab60f8ce09fd7cad1c0c301
ngocduydinh <duydn@hyperlogy.com>
2024-01-28 22:31:11 +0700

    slide bar mock up and basic layout

---
+++ src/app/search/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+import React from "react";
+
+type Props = {};
+export const metadata: Metadata = {
+  title: "Search",
+};
+const SearchPage = (props: Props) => {
+  return <div>SearchPage</div>;
+};
+
+export default SearchPage;
